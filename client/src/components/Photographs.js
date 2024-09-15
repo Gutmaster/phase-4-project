cac8c9c917695cdee81005d3fa479b3f728440e2
+ <+>
2024-09-15 14:56:27 -0700

    added sorting by type in photographs

--- client/src/components/Photographs.js
+++ client/src/components/Photographs.js
@@ -2,9 +2,24 @@ import { useEffect, useState } from "react";
 import Photograph from "./Photograph.js"
 
 function Photographs() {
-  const [photos, setPhotos] = useState([]);
-  const [filterMode, setFilterMode] = useState('nofilter');
+  const [photos, setPhotos] = useState([])
+  const [filterMode, setFilterMode] = useState('nofilter')
+  const [filter, setFilter] = useState('nofilter')
+  const [animals, setAnimals] = useState([]);
+  const [locations, setLocations] = useState([])
 
+  useEffect(() => {
+    fetch("/animals")
+      .then((r) => r.json())
+      .then(json => setAnimals(json));
+  }, []);
+
+  useEffect(() => {
+    fetch("/locations")
+     .then((r) => r.json())
+     .then(json => setLocations(json));
+  }, []);
+  
   useEffect(() => {
     fetch("/photographs")
       .then((r) => r.json())
@@ -23,9 +38,14 @@ function Photographs() {
     });
   }
   
-  const handleFilterTypeChange = (event) => {
-    setFilterMode(event.target.value);
-  };
+  const handleFilterTypeChange = (event) => setFilterMode(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const filteredPhotos = photos.filter((photo) => 
+    (filterMode === 'animal' && photo.animal.name === filter) ||
+    (filterMode === 'location' && photo.location.name === filter) ||
+    filterMode === 'nofilter' || filter === 'nofilter'
+  )
 
   return (
     <section className="container">
@@ -34,9 +54,20 @@ function Photographs() {
         <option value="animal">Animal</option>
         <option value="location">Location</option>
       </select>
-      <p>Selected: {filterMode}</p>
 
-      {photos.map((photo) => (
+      {filterMode === 'nofilter' ? (<></>) : (
+        <div>
+          <select value={filter} onChange={handleFilterChange}>
+            <option value="nofilter">None</option>
+            {filterMode === 'animal' ? animals.map(animal => (
+              <option key={animal.id} value={animal.name}>{animal.name}</option>
+            )) : locations.map((location) => (
+              <option key={location.id} value={location.name}>{location.name}</option>))}
+          </select>
+        </div>
+      )}
+      
+      {filteredPhotos.map((photo) => (
         <Photograph key = {photo.id} animal = {photo.animal} image = {photo.image} location = {photo.location} datetime = {photo.datetime}/>
       ))}
     </section>
--- server/seed.py
+++ server/seed.py
@@ -26,10 +26,10 @@ if __name__ == '__main__':
         Location.query.delete()
 
         print("Creating Species...")
-        animal1 = Animal(name="Frog")
-        animal2 = Animal(name="Bear")
-        animal3 = Animal(name="Cat")
-        animals = [animal1, animal2, animal3]
+        frog = Animal(name="Frog")
+        bear = Animal(name="Bear")
+        cat = Animal(name="Cat")
+        animals = [frog, bear, cat]
 
         print("Creating Locations...")
         location1 = Location(name="Forest")
@@ -41,10 +41,11 @@ if __name__ == '__main__':
         dt_formatted = f"{dt.day}/{dt.month}/{dt.year} {dt.hour}:{dt.minute:02d}"
         
         print("Creating Sightings...")
-        sighting1 = Photograph(datetime = dt_formatted, animal = animal1, location = location1, image = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-JfVO_Hwqlb4%2FTlOjgETXGnI%2FAAAAAAAAAc4%2FKYiJ_rRA5vA%2Fs1600%2Ftree_frog_3.jpg&f=1&nofb=1&ipt=0c45bdee3a92168ace54b7573250e4d7321008620f864be8300bca996debcc6a&ipo=images')
-        sighting2 = Photograph(datetime = dt_formatted, animal = animal2, location = location2, image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages6.alphacoders.com%2F529%2Fthumb-1920-529903.jpg&f=1&nofb=1&ipt=cae0c6abdc08cea8c14a123c2949b0c8069a392371af2f99371f2b04f1cbc1c5&ipo=images')
-        sighting3 = Photograph(datetime = dt_formatted, animal = animal3, location = location3, image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.the-sun.com%2Fwp-content%2Fuploads%2Fsites%2F6%2F2023%2F10%2FKS-crunchy-cat-comp.jpg%3Fw%3D620&f=1&nofb=1&ipt=260c156d905b4d1a42dba3496bcc1c0a119e0fcb2c63a289040eafd20500ca4c&ipo=images')
-        sightings = [sighting1, sighting2, sighting3]
+        sighting1 = Photograph(datetime = dt_formatted, animal = frog, location = location1, image = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-JfVO_Hwqlb4%2FTlOjgETXGnI%2FAAAAAAAAAc4%2FKYiJ_rRA5vA%2Fs1600%2Ftree_frog_3.jpg&f=1&nofb=1&ipt=0c45bdee3a92168ace54b7573250e4d7321008620f864be8300bca996debcc6a&ipo=images')
+        sighting2 = Photograph(datetime = dt_formatted, animal = bear, location = location2, image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages6.alphacoders.com%2F529%2Fthumb-1920-529903.jpg&f=1&nofb=1&ipt=cae0c6abdc08cea8c14a123c2949b0c8069a392371af2f99371f2b04f1cbc1c5&ipo=images')
+        sighting3 = Photograph(datetime = dt_formatted, animal = cat, location = location3, image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.the-sun.com%2Fwp-content%2Fuploads%2Fsites%2F6%2F2023%2F10%2FKS-crunchy-cat-comp.jpg%3Fw%3D620&f=1&nofb=1&ipt=260c156d905b4d1a42dba3496bcc1c0a119e0fcb2c63a289040eafd20500ca4c&ipo=images')
+        photo4 = Photograph(datetime = dt_formatted, animal = bear, location = location3, image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fv.w-x.co%2F1654795237283_060922_CLIMATE_CHANGE_COULD_LEAD_TO_PIZZLY_BEARSprproj.jpg&f=1&nofb=1&ipt=21de8508a57162226ee02f13200b2f4c631c094de575a6096b5f0894d2dc92a4&ipo=images')
+        sightings = [sighting1, sighting2, sighting3, photo4]
 
         db.session.add_all(animals)
         db.session.add_all(locations)
