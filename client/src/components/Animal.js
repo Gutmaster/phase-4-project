f2cac18ba3fb1b50fddc405aacb00fcb7e21ab15
+ <+>
2024-09-17 08:19:24 -0700

    added scrolling through pictures for animals tab

--- client/src/components/Animal.js
+++ client/src/components/Animal.js
@@ -1,21 +1,29 @@
-import {useEffect, useState} from "react";
-import { Link } from "react-router-dom";
+import {useState} from "react";
 
 const noImage = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.shoshinsha-design.com%2Fwp-content%2Fuploads%2F2020%2F05%2Fnoimage-760x460.png&f=1&nofb=1&ipt=d872ea62b4b151bf09b2fbf210849cba33aa79c637b5c2ce34dd1d2399081e1b&ipo=images'
 
 function Animal({animal}) {
   const [photoIndex, setPhotoIndex] = useState(0)
 
-
+  function handleArrowRight(e) {
+    e.preventDefault()
+    setPhotoIndex((photoIndex + 1) % animal.photographs.length)
+  }
+  function handleArrowLeft(e) {
+    e.preventDefault()
+    setPhotoIndex((photoIndex - 1 + animal.photographs.length) % animal.photographs.length)
+  }
 
   return (
     <div className="photoCard">
       <h4>
-        {/* <Link to={`/animals/${animal.id}`}> Name: {animal.name}</Link> */}
         <p>{animal.name}</p>
       </h4>
-      {animal.photographs.length ? <img src={animal.photographs[0].image}/> : <img src={noImage}/>}
-      
+      <span>
+        <button onClick={handleArrowLeft}>&lt;</button>
+        {animal.photographs.length ? <img src={animal.photographs[photoIndex].image}/> : <img src={noImage}/>}
+        <button onClick={handleArrowRight}>&gt;</button>
+      </span>
       <p>{animal.description}</p>
     </div>
   );
