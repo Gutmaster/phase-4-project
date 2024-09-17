0369c2aa2f2981adba2f552473df96bde15e03d7
+ <+>
2024-09-17 08:03:48 -0700

    added empty image placeholders for animals and locations with no remaining photographs

--- client/src/components/Animal.js
+++ client/src/components/Animal.js
@@ -1,8 +1,11 @@
 import {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 
+const noImage = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.shoshinsha-design.com%2Fwp-content%2Fuploads%2F2020%2F05%2Fnoimage-760x460.png&f=1&nofb=1&ipt=d872ea62b4b151bf09b2fbf210849cba33aa79c637b5c2ce34dd1d2399081e1b&ipo=images'
+
 function Animal({animal}) {
   const [photoIndex, setPhotoIndex] = useState(0)
+
 
 
   return (
@@ -11,7 +14,8 @@ function Animal({animal}) {
         {/* <Link to={`/animals/${animal.id}`}> Name: {animal.name}</Link> */}
         <p>{animal.name}</p>
       </h4>
-      <img src={animal.photographs[0].image}/>
+      {animal.photographs.length ? <img src={animal.photographs[0].image}/> : <img src={noImage}/>}
+      
       <p>{animal.description}</p>
     </div>
   );
--- client/src/components/Location.js
+++ client/src/components/Location.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+const noImage = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.shoshinsha-design.com%2Fwp-content%2Fuploads%2F2020%2F05%2Fnoimage-760x460.png&f=1&nofb=1&ipt=d872ea62b4b151bf09b2fbf210849cba33aa79c637b5c2ce34dd1d2399081e1b&ipo=images'
 
 function Location({location}) {
   return (
     <div className="photoCard">
       <h4>
-        <Link to={`/location/${location.id}`}> Name: {location.name}</Link>
+        <p>{location.name}</p>
       </h4>
-      <img src={location.photographs[0].image}/>
+      {location.photographs.length ? <img src={location.photographs[0].image}/> : <img src={noImage}/>}
     </div>
   );
 }
