230417fb4aad9b987140389f8bb645aa5470b3eb
+ <+>
2024-09-19 15:02:52 -0700

    alt_text fix forreal

--- client/src/components/Photograph.js
+++ client/src/components/Photograph.js
@@ -4,7 +4,7 @@ function Photograph({id, animal, location, datetime, image, handleDelete}) {
   return (
     <div className="photoCard">
       <h4>Name: {animal.name}</h4>
-      <img src={image}/>
+      <img src={image} alt={animal.name}/>
       <h4>Location: {location.name}</h4>
       <h5>Date: {datetime}</h5>
       <button onClick={() => handleDelete(id)}>Delete</button>
