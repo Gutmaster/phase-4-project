035a9279f9361907d2740a6c97b810f81cc3feba
+ <+>
2024-10-06 09:20:57 -0700

    added notification popup for successful photo submission

--- client/src/components/NewPhoto.js
+++ client/src/components/NewPhoto.js
@@ -6,6 +6,11 @@ function NewPhoto({animals, locations, photos, setPhotos}) {
     const [image, setImage] = useState('')
     const [newAnimal, setNewAnimal] = useState('')
     const [newLocation, setNewLocation] = useState('')
+    const [alertMessage, setAlertMessage] = useState('')
+
+    function alertReset(){
+        setAlertMessage('')
+    }
 
     async function onSubmit(event) {
         console.log('SUBMITTING')
@@ -34,6 +39,8 @@ function NewPhoto({animals, locations, photos, setPhotos}) {
             setAnimal('select')
             setLocation('select')
             setImage('')
+            setAlertMessage('Photo added!')
+            setTimeout(alertReset, 2000)
         } catch (error) {
             // This block will catch network errors and other unexpected issues
             console.error('Network Error or unexpected issue:', error)
@@ -41,35 +48,38 @@ function NewPhoto({animals, locations, photos, setPhotos}) {
     }
 
     return (
-        <form className='newPhotoForm' onSubmit={onSubmit}>
-            <div className='left'>
-                <label htmlFor='animal'>Animal: </label>
-                    <select id="animal" name="animal" value={animal} onChange={(e) => setAnimal(e.target.value)}>
-                        <option value={'select'}>Select Animal</option>
-                        {animals.map((animal) => (
-                            <option key={animal.id} value={animal.name}>{animal.name}</option>
-                        ))}
-                        <option value={'newAnimal'}>New Animal</option>
-                    </select>
+        <div>
+            <form className='newPhotoForm' onSubmit={onSubmit}>
+                <div className='left'>
+                    <label htmlFor='animal'>Animal: </label>
+                        <select id="animal" name="animal" value={animal} onChange={(e) => setAnimal(e.target.value)}>
+                            <option value={'select'}>Select Animal</option>
+                            {animals.map((animal) => (
+                                <option key={animal.id} value={animal.name}>{animal.name}</option>
+                            ))}
+                            <option value={'newAnimal'}>New Animal</option>
+                        </select>
 
-                <label htmlFor='location'>Location: </label>
-                    <select id="location" name="location" value={location} onChange={(e) => setLocation(e.target.value)}>
-                        <option value={'select'}>Select Location</option>
-                        {locations.map((location) => (
-                            <option key={location.id} value={location.name}>{location.name}</option>
-                        ))}
-                        <option value={'newLocation'}>New Location</option>
-                    </select>
+                    <label htmlFor='location'>Location: </label>
+                        <select id="location" name="location" value={location} onChange={(e) => setLocation(e.target.value)}>
+                            <option value={'select'}>Select Location</option>
+                            {locations.map((location) => (
+                                <option key={location.id} value={location.name}>{location.name}</option>
+                            ))}
+                            <option value={'newLocation'}>New Location</option>
+                        </select>
 
-                <label htmlFor='image'>Image URL: </label>
-                    <input type="text" id = "image" name="image" value={image} onChange={(e) => setImage(e.target.value)}/>
-            </div>
-            <div className='right'>
-                {animal === 'newAnimal' ? <input type="text" id = "newAnimal" name="newAnimal" value={newAnimal} onChange={(e) => setNewAnimal(e.target.value)}/> : <input type="text" style = {{visibility: 'hidden'}}/>}
-                {location === 'newLocation' ? <input type="text" id = "newLocation" name="newLocation" value={newLocation} onChange={(e) => setNewLocation(e.target.value)}/> : <input type="text" style = {{visibility: 'hidden'}}/>}
-            </div>  
-            <button type="submit" className='submitButton'>Submit</button>
-        </form>
+                    <label htmlFor='image'>Image URL: </label>
+                        <input type="text" id = "image" name="image" value={image} onChange={(e) => setImage(e.target.value)}/>
+                </div>
+                <div className='right'>
+                    {animal === 'newAnimal' ? <input type="text" id = "newAnimal" name="newAnimal" value={newAnimal} onChange={(e) => setNewAnimal(e.target.value)}/> : <input type="text" style = {{visibility: 'hidden'}}/>}
+                    {location === 'newLocation' ? <input type="text" id = "newLocation" name="newLocation" value={newLocation} onChange={(e) => setNewLocation(e.target.value)}/> : <input type="text" style = {{visibility: 'hidden'}}/>}
+                </div>  
+                <button type="submit" className='submitButton'>Submit</button>
+            </form>
+            {alertMessage!==''? <p className='alert'>{alertMessage}</p>: <></>}
+        </div>
     );
 }
 
