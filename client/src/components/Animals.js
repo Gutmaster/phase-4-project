6fb7b4ec4f668cd43a58624b36ccba9ee396052f
+ <+>
2024-10-29 11:36:05 -0700

    added bg images for animal and location pages

--- client/src/components/Animals.js
+++ client/src/components/Animals.js
@@ -2,13 +2,15 @@ import Animal from "./Animal.js"
 
 function Animals({animals}) {
   return (
-    <section className="container">
-      {animals.map((animal) => (
-        <div key = {animal.id}>
-            <Animal animal = {animal}/>
-        </div>
-      ))}
-    </section>
+    <div className='animals'>
+      <section className="container">
+        {animals.map((animal) => (
+          <div key = {animal.id}>
+              <Animal animal = {animal}/>
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
 
--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -2,13 +2,15 @@ import Location from "./Location.js"
 
 function Locations({locations}) {
   return (
-    <section className="container">
-      {locations.map((location) => (
-        <div key = {location.id}>
-            <Location location = {location}/>
-        </div>
-      ))}
-    </section>
+    <div className='locations'>
+      <section className="container">
+        {locations.map((location) => (
+          <div key = {location.id}>
+              <Location location = {location}/>
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
 
